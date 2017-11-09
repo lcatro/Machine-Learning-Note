194d10190c45a1db5f7e37b46986b999156da326
LCatro <lcatro@sina.cn>
2017-11-09 18:38:39 +0800

    update learning logic

--- captcha/sklearn-captcha.py
+++ captcha/sklearn-captcha.py
@@ -8,6 +8,7 @@ from PIL import Image
 
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 
 from captcha_process import *
 
@@ -24,15 +25,17 @@ def image_convert_to_array(image_data,image_width,image_height) :
         for height_index in range(image_height) :
             pixel_list_data.append(abs(image_data[width_index,height_index] - 128))
             
-        pixel_data.append(pixel_list_data)
+        #pixel_data.append(pixel_list_data)
+        pixel_data += pixel_list_data
     
     return array(pixel_data)
 
 def load_image(path) :
     image = Image.open(path)
     image_data = image.load()
+    image_data = image_convert_to_array(image_data,image.size[0],image.size[1])
     
-    return image_convert_to_array(image_data,image.size[0],image.size[1])
+    return image_data
 
 def load_sample() :
     sample_files = listdir(sample_dir)
@@ -47,13 +50,10 @@ def load_sample() :
         data_list.append(image_data)
         classfy_list.append(number_class)
         
-    #data_list = array(data_list)
-    #data_list = data_list.reshape((len(data_list), -1))
-        
     return array(data_list) , array(classfy_list)
 
 def train_model(data_list,classfy_list) :
-    svc_model = SVC(gamma = 0.001)
+    svc_model = SVC(gamma = 0.001,C = 0.002)
     
     svc_model.fit(data_list,classfy_list)
     
@@ -65,11 +65,11 @@ def try_classfy(train_model,image_data) :
 def load_captcha(path = 'get_captcha.png',captcha_count = 4) :
     image = preprocess_captcha(path)
     image_width = image.size[0]
-    image = image.resize((image_width,32),Image.ANTIALIAS)
+    image = image.resize((image_width / 2,32 / 2),Image.ANTIALIAS)
     image_data = []
     
     for index in range(captcha_count) :  #  split 4 char
-        image_char = image.crop((index * 32,0,(index + 1) * 32,32))
+        image_char = image.crop((index * 32 / 2,0,(index + 1) * 32 / 2,32 / 2))
         
         image_data.append(image_convert_to_array(image_char.load(),image_char.size[0],image_char.size[1]))
     
@@ -77,9 +77,8 @@ def load_captcha(path = 'get_captcha.png',captcha_count = 4) :
 
 if __name__ == '__main__' :
     start_time = time()
-    data_list,classfy_list = load_sample()
     
-    print data_list,classfy_list , len(data_list) ,len(data_list[0])
+    data_list,classfy_list = load_sample()
     
     print 'Load Data Success' , time() - start_time
     
@@ -91,8 +90,9 @@ if __name__ == '__main__' :
     
     print 'Load Captche Success' , time() - start_time
     
-    for image_index in image_data :
-        print try_classfy(svc_model,image_index) ,
+    #for image_index in image_data :
+    #    print try_classfy(svc_model,image_index) ,
+    print try_classfy(svc_model,image_data)
         
     print 'Using Time :' , time() - start_time
         
